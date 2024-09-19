0d9ee712e7dafdc044a812d5c70bef522ca87a63
Matěj Boška <matej0boska@gmail.com>
2024-09-19 21:27:31 +0200

    initial commit

---
+++ GUI-WA-Masopust/guiwamasopust/BaseGetData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace http_winform
+{
+    [JsonPolymorphic]
+    [JsonDerivedType(typeof(GetPeopleData))]
+    [JsonDerivedType(typeof(GetTypeData))]
+    public class BaseGetData
+    {
+        [JsonPropertyName("ID")]
+        public int ID { get; set; }
+    }
+}
---
+++ GUI-WA-Masopust/guiwamasopust/GetTypeData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace http_winform
+{
+    [JsonPolymorphic]
+    [JsonDerivedType(typeof(GetPeopleData), "folder")]
+    [JsonDerivedType(typeof(GetTypeData), "test")]
+    public class GetTypeData : BaseGetData
+    {
+        [JsonPropertyName("typ")]
+        public string Typ { get; set; }
+    }
+}
